7dff192ebe8250fcda45ced4fd38b92b4ac37c47
Sputnik Spacemen <4946877+sreekumartp@users.noreply.github.com>
2025-03-28 07:28:30 +0530

    Refactor EventBus to implement a thread pool for event processing and improve subscriber management

--- include/event_bus.h
+++ include/event_bus.h
@@ -1,28 +1,45 @@
 #ifndef EVENT_BUS_H
 #define EVENT_BUS_H
 
-#include "events.h"
-#include <functional>
+#include <unordered_map>
 #include <vector>
-#include <map>
+#include <functional>
+#include <queue>
 #include <mutex>
+#include <condition_variable>
+#include <thread>
+#include "events.h"
+
+#define MAX_THREAD_POOL_SIZE 16
 
 class EventBus {
-private:
-    std::map<EventType, std::vector<std::function<void(const EventData&)>>> subscribers_;
-    std::mutex mutex_;
-
-    // Singleton pattern (private constructor and static instance)
-    EventBus() = default;
-    ~EventBus() = default;
-    EventBus(const EventBus&) = delete;
-    EventBus& operator=(const EventBus&) = delete;
-
 public:
     static EventBus& instance();
 
-    void subscribe(EventType type, std::function<void(const EventData&)> callback);
+    using Subscriber = std::function<void(const EventData&)>;
+    void subscribe(EventType type, Subscriber subscriber);
     void publish(const EventData& event);
+
+private:
+    EventBus();
+    ~EventBus();
+    EventBus(const EventBus&) = delete;
+    EventBus& operator=(const EventBus&) = delete;
+
+    void startThreadPool();
+    void stopThreadPool();
+    void notifySubscribers(const EventData& event);
+
+    // Thread pool
+    std::vector<std::thread> threadPool;
+    std::queue<EventData> eventQueue;
+    std::mutex queueMutex;
+    std::condition_variable queueCondition;
+    bool stop;
+
+    // Subscribers
+    std::unordered_map<EventType, std::vector<Subscriber>> subscribers;
+    std::mutex subscribersMutex;
 };
 
 #endif // EVENT_BUS_H
--- src/event_bus.cpp
+++ src/event_bus.cpp
@@ -1,26 +1,81 @@
 #include "event_bus.h"
-#include <iostream>
-#include <thread>
 
 EventBus& EventBus::instance() {
-    static EventBus instance_;
-    return instance_;
+    static EventBus instance;
+    return instance;
 }
 
-void EventBus::subscribe(EventType type, std::function<void(const EventData&)> callback) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    subscribers_[type].push_back(callback);
-    std::cout << "Event Subscription " << std::this_thread::get_id() << std::endl;
-    
+EventBus::EventBus() : stop(false) {
+    startThreadPool();
+}
+
+EventBus::~EventBus() {
+    stopThreadPool();
+}
+
+void EventBus::subscribe(EventType type, Subscriber subscriber) {
+    std::lock_guard<std::mutex> lock(subscribersMutex);
+    subscribers[type].push_back(subscriber);
 }
 
 void EventBus::publish(const EventData& event) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    if (subscribers_.count(event.type)) {
-        for (const auto& callback : subscribers_[event.type]) {
-            std::cout << "Event Publish " << std::this_thread::get_id() << std::endl;
-    
-            callback(event);
+    {
+        std::lock_guard<std::mutex> lock(queueMutex);
+        eventQueue.push(event);
+    }
+    queueCondition.notify_one();
+}
+
+void EventBus::startThreadPool() {
+    // const size_t threadCount = std::thread::hardware_concurrency();
+    const size_t threadCount = std::min(std::thread::hardware_concurrency(), static_cast<unsigned int>(MAX_THREAD_POOL_SIZE));
+    for (size_t i = 0; i < threadCount; ++i) {
+        threadPool.emplace_back([this]() {
+            while (true) {
+                EventData event;
+                {
+                    std::unique_lock<std::mutex> lock(queueMutex);
+                    queueCondition.wait(lock, [this]() { return stop || !eventQueue.empty(); });
+
+                    if (stop && eventQueue.empty()) {
+                        return;
+                    }
+
+                    event = eventQueue.front();
+                    eventQueue.pop();
+                }
+
+                // Notify subscribers
+                notifySubscribers(event);
+            }
+        });
+    }
+}
+
+void EventBus::stopThreadPool() {
+    {
+        std::lock_guard<std::mutex> lock(queueMutex);
+        stop = true;
+    }
+    queueCondition.notify_all();
+
+    for (std::thread& thread : threadPool) {
+        if (thread.joinable()) {
+            thread.join();
         }
+    }
+}
+
+void EventBus::notifySubscribers(const EventData& event) {
+    std::vector<Subscriber> eventSubscribers;
+    {
+        std::lock_guard<std::mutex> lock(subscribersMutex);
+        if (subscribers.find(event.type) != subscribers.end()) {
+            eventSubscribers = subscribers[event.type];
+        }
+    }
+
+    for (const auto& subscriber : eventSubscribers) {
+        subscriber(event);
     }
 }
